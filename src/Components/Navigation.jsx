9ebe75a9f2d239d86b7b9f862fd2abe9acd9592d
Iko Almas <90682984+IkoAlmasDevGame@users.noreply.github.com>
2023-01-07 14:41:19 +0700

    Add files via upload

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Container, Nav, Navbar} from 'react-bootstrap';
 import svg from '../Assets/logo/logo192.png';
 
@@ -34,41 +33,4 @@ const NavigationBar = () => {
     );
 }
 
-=======
-import { Container, Nav, Navbar} from 'react-bootstrap';
-import svg from '../Assets/logo/logo192.png';
-
-const NavigationBar = () => {
-    return(
-    <header style={{backgroundColor : "#EF9A53"}}>
-        <div>   
-            <Navbar variant='white' expand='lg' style={{fontSize : "medium", fontWeight : "500", letterSpacing : "1px"}}>
-                <Container>
-                <img src={svg} alt='' style={{width : "64px", height : "64px"}}/>
-                    <Navbar.Brand style={{color : "whitesmoke", fontWeight : "700", fontSize : "large",
-                    fontFamily : "arial", textShadow : "0px 1px 1px black"}}>My News Private</Navbar.Brand>
-                        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                            <Navbar.Collapse className="collapse navbar-collapse" id="navbarSupportedContent"
-                            style={{color : "white"}}>
-                                <Nav style={{margin : "auto"}}>
-                                    <Nav.Link style={{color : "white", fontWeight : "500", textShadow : "2px 1px black"}} 
-                                    href='#home'>Home</Nav.Link>
-                                    <Nav.Link style={{color : "white", fontWeight : "500", textShadow : "2px 1px black"}} 
-                                    href=''>About</Nav.Link>
-                                    <Nav.Link style={{color : "white", fontWeight : "500", textShadow : "2px 1px black"}} 
-                                    href=''>Contact</Nav.Link>
-                                    <Nav.Link style={{color : "white", fontWeight : "500", textShadow : "2px 1px black"}} 
-                                    href='#terbaru'>Berita Baru</Nav.Link>
-                                    <Nav.Link style={{color : "white", fontWeight : "500", textShadow : "2px 1px black"}} 
-                                    href=''>Berita Lama</Nav.Link>
-                                </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </div>
-    </header>
-    );
-}
-
->>>>>>> d90cea10dfce5931b9dcff859333cb9d37423843
 export default NavigationBar
